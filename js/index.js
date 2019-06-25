85ad937d635586d2c2020cd1f56fac4cdcb1f6e9
laliao <897069154@qq.com>
2019-06-25 13:56:14 +0800

    js2

--- js/index.js
+++ js/index.js
@@ -4,7 +4,9 @@ let imglist=document.querySelectorAll(".lunb .cent .center .tu a")
 let dianlist=document.querySelectorAll(".lunb .cent .center .dian li")
 let bglist=document.querySelectorAll(".lunb .lbb .back")
 let bigbox=document.querySelector(".lunb")
-
+let ljlist=document.querySelectorAll(".lunb .cent .center .jian>span")
+let lj=document.querySelector(".lunb .cent .center .jian")
+let tuz=document.querySelector(".lunb .cent .center .tu")
 let index=0;
 imglist[index].style.opacity=1;
 dianlist[index].classList.add("hot")
@@ -42,13 +44,38 @@ dianlist.forEach((v,i)=>{
         v.classList.add("hot")
         bglist[index].style.opacity=1;
     }
-
 })
+tuz.onmouseenter=()=>{
+    lj.style.display="block"
+}
+lj.onmouseover=()=>{
+    lj.style.display="block"
+}
+tuz.onmouseleave=()=>{
+    lj.style.display="none"
+}
+ljlist[1].onclick=()=>{
+move()
+}
+ljlist[0].onclick=()=>{
+    index--
+    if(index<0){index=imglist.length-1}
+    imglist.forEach((v,i)=>{
+        v.style.opacity=0;
+        dianlist[i].classList.remove("hot")
+        bglist[i].style.opacity=0;
+    })
+    imglist[index].style.opacity=1;
+    dianlist[index].classList.add("hot")
+    bglist[index].style.opacity=1;
+}
 
 
-/*主图左侧导航 */
+
+/*主图左侧导航,选项卡 */
 let conlist=document.querySelectorAll(".lunb .cent .left .daoh .sort-detail")
 let dhlist=document.querySelectorAll(".lunb .cent .left .daoh>li")
+let bigld=document.querySelector(".lunb .cent .left .daoh")
 
 dhlist.forEach((v,i)=>{
     v.onmouseover=()=>{
@@ -66,17 +93,23 @@ conlist.forEach((v,i)=>{
         dhlist.forEach((a)=>{a.classList.remove("hot1")})
         dhlist[i].classList.add("hot1")
     }
-    v.onmouseout=()=>{
+    // v.onmouseout=()=>{
+    //     v.style.display="none"
+    //     dhlist[i].classList.remove("hot1")
+    // }
+})
+bigld.onmouseout=()=>{
+    conlist.forEach((v,i)=>{
         v.style.display="none"
         dhlist[i].classList.remove("hot1")
-    }
-})
-
+    })
+}
 
 //楼层跳转
 let btnlist=document.querySelectorAll(".leftdh .floatBar .list>li")
 let tzclist=document.querySelectorAll(".lcc")
 let btn=document.querySelector(".leftdh .floatBar")
+let back=document.querySelector(".leftdh .floatBar .return")
 let arr=[]
 tzclist.forEach((v,i)=>{arr.push(v.offsetTop)})    //offsetTop
 let tiaoz=null
@@ -90,7 +123,7 @@ btnlist.forEach((v,i)=>{
         tiaoz=setInterval(()=>{
             dqTop=document.documentElement.scrollTop
             speed=(arr[i]-document.documentElement.scrollTop)/10
-            speed>0?Math.ceil(speed):Math.floor(speed)
+            speed=speed>0?Math.ceil(speed):Math.floor(speed)
             dqTop+=speed
             scrollTo(0,dqTop)
             if(dqTop==arr[i]){clearInterval(tiaoz)}
@@ -109,7 +142,18 @@ window.onscroll=()=>{
             v.classList.add("first")
         }
     })  
-
+}
+back.onclick=()=>{
+    clearInterval(timer)
+    timer=setInterval(()=>{
+        scrollt=document.documentElement.scrollTop 
+        speed=Math.floor((0-scrollt)/10)
+        scrollt+=speed
+        scrollTo(0,scrollt)
+        if(scrollt==0){
+            clearInterval(timer)
+        }
+    },20)
 }
 
 
